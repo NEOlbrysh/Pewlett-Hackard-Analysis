28214b8453ef4661010a97e53406f3b994aefc39
NEOlbrysh <103701561+NEOlbrysh@users.noreply.github.com>
2022-06-19 17:50:53 -0400

    Add files via upload

--- schema.sql
+++ schema.sql
@@ -1,11 +1,12 @@
 -- Creating tables for PH-NicoleDB
 
 CREATE TABLE departments (
-     dept_no VARCHAR(4) NOT NULL,
-     dept_name VARCHAR(40) NOT NULL,
+     dept_no VARCHAR NOT NULL,
+     dept_name VARCHAR NOT NULL,
      PRIMARY KEY (dept_no),
      UNIQUE (dept_name)
 );
+
 
 CREATE TABLE Employess(
      emp_no INT NOT NULL,
@@ -18,7 +19,7 @@ CREATE TABLE Employess(
 );
 
 CREATE TABLE Managers (
-dept_no VARCHAR(4) NOT NULL,
+dept_no VARCHAR NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -36,4 +37,68 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
+CREATE TABLE title (
+  emp_no INT NOT NULL,
+  title VARCHAR NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employess (emp_no),
+  PRIMARY KEY (emp_no)
+);
+
 SELECT * from Departments
+
+DROP TABLE departments CASCADE;
+DROP TABLE employees CASCADE;
+DROP TABLE managers CASCADE;
+DROP TABLE salaries CASCADE;
+DROP TABLE title CASCADE;
+
+COPY employees(emp_no,birth_date,first_name,last_name,gender,hire_date)
+FROM "C:\Users\n_olb\Downloads\employees.csv"
+DELIMITER ","
+CSV HEADER;
+C:\Users\n_olb\Downloads\employees.csv
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
